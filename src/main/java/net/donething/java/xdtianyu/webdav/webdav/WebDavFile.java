0aa54ad4d2d261f35f3e6cb2a9eea3ceb0969c8e
Donething <donething.net@gmail.com>
2018-02-03 09:14:43 +0800

    1. 添加将远程文件信息填充到WebDAVFile实例里的方法

--- src/main/java/net/donething/java/xdtianyu/webdav/webdav/WebDavFile.java
+++ src/main/java/net/donething/java/xdtianyu/webdav/webdav/WebDavFile.java
@@ -13,6 +13,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Field;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLDecoder;
@@ -25,9 +26,13 @@ import java.util.List;
 
 public class WebDavFile {
     public final static String TAG = WebDavFile.class.getSimpleName();
+    public final static String OBJECT_NOT_EXISTS_TAG = "ObjectNotFound";
+    // 指定返回哪些属性
     private final static String DIR = "<?xml version=\"1.0\"?>\n" +
             "<a:propfind xmlns:a=\"DAV:\">\n" +
-            "<a:prop><a:resourcetype/></a:prop>\n" +
+            "<a:prop>\n" +
+            "<a:displayname/>\n<a:resourcetype/>\n<a:getcontentlength/>\n<a:creationdate/>\n<a:getlastmodified/>\n%s" +
+            "</a:prop>\n" +
             "</a:propfind>";
 
     private URL url;
@@ -38,6 +43,7 @@ public class WebDavFile {
     private long lastModified;
     private long size;
     private boolean isDirectory = true;
+    private boolean exists = false;
     private String parent = "";
     private String urlName = "";
 
@@ -67,10 +73,86 @@ public class WebDavFile {
         return url.toString();
     }
 
+    /**
+     * 填充文件信息。实例化WebDAVFile对象时，并没有将远程文件的信息填充到实例中。需要手动填充！
+     *
+     * @return 远程文件是否存在
+     */
+    public boolean indexFileInfo() {
+        Response response = propFindResponse(new ArrayList<>());
+        String s = "";
+        try {
+            if (response == null || !response.isSuccessful()) {
+                this.exists = false;
+                return false;
+            }
+            s = response.body().string();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Serializer serializer = new Persister();
+        try {
+            MultiStatus multiStatus = serializer.read(MultiStatus.class, s);
+            net.donething.java.xdtianyu.webdav.webdav.model.Response res = multiStatus.getResponse().get(0);
+            Prop prop = res.getPropstat().getProp();
+            this.canon = prop.getDisplayname();
+            this.createTime = 0;
+            this.lastModified = 0;
+            this.size = prop.getGetcontentlength();
+            this.isDirectory = prop.getResourcetype().getCollection() != null;
+            this.exists = true;
+            // this.parent = "";
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 列出当前路径下的文件。默认列出文件的如下属性：displayname、resourcetype、getcontentlength、creationdate、getlastmodified
+     *
+     * @return 文件列表
+     */
     public WebDavFile[] listFiles() {
+        return listFiles(new ArrayList<>());
+    }
+
+    /**
+     * 列出当前路径下的文件
+     *
+     * @param propsList 指定列出文件的哪些属性
+     * @return 文件列表
+     */
+    public WebDavFile[] listFiles(ArrayList<String> propsList) {
+        Response response = propFindResponse(propsList);
+        try {
+            if (response.isSuccessful()) {
+                return parseDir(response.body().string());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new WebDavFile[0];
+    }
+
+    private Response propFindResponse(ArrayList<String> propsList) {
+        StringBuilder requestProps = new StringBuilder();
+        for (String p : propsList) {
+            requestProps.append("<a:" + p + "/>\n");
+        }
+        String requestPropsStr;
+        if (requestProps.toString().isEmpty()) {
+            requestPropsStr = DIR.replace("%s", "");
+        } else {
+            requestPropsStr = String.format(DIR, requestProps.toString() + "\n");
+        }
         Request.Builder request = new Request.Builder()
                 .url(getUrl())
-                .method("PROPFIND", RequestBody.create(MediaType.parse("text/plain"), DIR));
+                // 添加RequestBody对象，可以只返回的属性。如果设为null，则会返回全部属性
+                // 注意：尽量手动指定需要返回的属性。若返回全部属性，可能后由于Prop.java里没有该属性名，而崩溃。
+                .method("PROPFIND", RequestBody.create(MediaType.parse("text/plain"), requestPropsStr));
 
         HttpAuth.Auth auth = HttpAuth.getAuth(url.toString());
         if (auth != null) {
@@ -78,15 +160,44 @@ public class WebDavFile {
         }
 
         try {
-            Response response = okHttpClient.newCall(request.build()).execute();
-            String s = response.body().string();
-            return parseDir(s);
+            return okHttpClient.newCall(request.build()).execute();
         } catch (Exception e) {
             //catch (IOException | XmlPullParserException | IllegalArgumentException e)
             e.printStackTrace();
         }
-
         return null;
+    }
+
+    private WebDavFile[] parseDir(String s) {
+        List<WebDavFile> list = new ArrayList<>();
+        Serializer serializer = new Persister();
+        try {
+            MultiStatus multiStatus = serializer.read(MultiStatus.class, s);
+            String parent = url.toString();
+            for (net.donething.java.xdtianyu.webdav.webdav.model.Response response : multiStatus.getResponse()) {
+                String path = url.getProtocol() + "://" + url.getHost() +
+                        (url.getPort() != -1 ? ":" + url.getPort() : "") +
+                        URLDecoder.decode(response.getHref().replace("+", "%2B"), "utf-8");
+                if (path.equalsIgnoreCase(parent)) {
+                    continue;
+                }
+
+                WebDavFile webDavFile = new WebDavFile(path);
+                Prop prop = response.getPropstat().getProp();
+                webDavFile.setCanon(prop.getDisplayname());
+                webDavFile.setCreateTime(0);
+                webDavFile.setLastModified(0);
+                webDavFile.setSize(prop.getGetcontentlength());
+                webDavFile.setIsDirectory(prop.getResourcetype().getCollection() != null);
+                webDavFile.exists(true);
+                webDavFile.setParent(parent);
+                list.add(webDavFile);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list.toArray(new WebDavFile[list.size()]);
     }
 
     public InputStream getInputStream() {
@@ -108,40 +219,11 @@ public class WebDavFile {
         return null;
     }
 
-    private WebDavFile[] parseDir(String s) throws Exception {  // XmlPullParserException, IOException
-
-        List<WebDavFile> list = new ArrayList<>();
-
-        Serializer serializer = new Persister();
-        try {
-            MultiStatus multiStatus = serializer.read(MultiStatus.class, s);
-            String parent = url.toString();
-            for (net.donething.java.xdtianyu.webdav.webdav.model.Response response : multiStatus.getResponse()) {
-                String path = url.getProtocol() + "://" + url.getHost() +
-                        (url.getPort() != -1 ? ":" + url.getPort() : "") +
-                        URLDecoder.decode(response.getHref().replace("+", "%2B"), "utf-8");
-
-                if (path.equalsIgnoreCase(parent)) {
-                    continue;
-                }
-
-                WebDavFile webDavFile = new WebDavFile(path);
-                Prop prop = response.getPropstat().getProp();
-                webDavFile.setCanon(prop.getDisplayname());
-                webDavFile.setCreateTime(0);
-                webDavFile.setLastModified(0);
-                webDavFile.setSize(prop.getGetcontentlength());
-                webDavFile.setIsDirectory(prop.getResourcetype().getCollection() != null);
-                webDavFile.setParent(parent);
-                list.add(webDavFile);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return list.toArray(new WebDavFile[list.size()]);
-    }
-
+    /**
+     * 根据自己的URL，在远程处创建对应的文件夹
+     *
+     * @return 是否创建成功
+     */
     public boolean makeAsDir() {
         Request.Builder request = new Request.Builder()
                 .url(getUrl())
@@ -150,9 +232,9 @@ public class WebDavFile {
     }
 
     /**
-     * 下载到本地
+     * 下载到本地。若本地存在同名文件，则取消下载
      *
-     * @param savedPath 保存路径
+     * @param savedPath 本地的完整路径，包括最后的文件名
      * @return 下载是否成功
      */
     public boolean download(String savedPath) {
@@ -219,6 +301,27 @@ public class WebDavFile {
         return false;
     }
 
+    /**
+     * 打印对象内的所有属性
+     */
+    public static <T> T printAllAttrs(String className, Object o) {
+        try {
+            Class<?> c = Class.forName(className);
+            Field[] fields = c.getDeclaredFields();
+            for (Field f : fields) {
+                f.setAccessible(true);
+            }
+            System.out.println("=============" + className + "===============");
+            for (Field f : fields) {
+                String field = f.toString().substring(f.toString().lastIndexOf(".") + 1); //取出属性名称
+                System.out.println(field + " --> " + f.get(o));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     public String getCanon() {
         return canon;
     }
@@ -280,7 +383,11 @@ public class WebDavFile {
     }
 
     public boolean exists() {
-        return true;
+        return exists;
+    }
+
+    public void exists(boolean exists) {
+        this.exists = exists;
     }
 
     public String getParent() {
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,6 +1,7 @@
 import net.donething.java.xdtianyu.webdav.http.HttpAuth;
 import net.donething.java.xdtianyu.webdav.webdav.WebDavFile;
-import okhttp3.MediaType;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by Donething on 2018/02/03
@@ -8,13 +9,31 @@ import okhttp3.MediaType;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("媒体类型：" + MediaType.parse(""));
         HttpAuth.addAuth("https://dav.jianguoyun.com/dav,", "donething@foxmail.com", "ajqx3p288w97q3cz");
         try {
-            WebDavFile file = new WebDavFile("https://dav.jianguoyun.com/dav/PrivBackup/测试文件夹22");
-            file.makeAsDir();
+            WebDavFile file = new WebDavFile("https://dav.jianguoyun.com/dav/PrivBackup");
+            file.indexFileInfo();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    // 打印对象内的所有属性
+    public static <T> T printAllAttrs(String s1, Object o) {
+        try {
+            Class<?> c = Class.forName(s1);
+            Field[] fields = c.getDeclaredFields();
+            for (Field f : fields) {
+                f.setAccessible(true);
+            }
+            System.out.println("=============" + s1 + "===============");
+            for (Field f : fields) {
+                String field = f.toString().substring(f.toString().lastIndexOf(".") + 1); //取出属性名称
+                System.out.println(field + " --> " + f.get(o));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
